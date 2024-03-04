17a04fd90307f4b20167b81f88a8fda7060eef4e
忘忧北萱草 <wybxc@qq.com>
2024-03-04 12:04:21 +0800

    fix: markdown format (#1)

--- src/bin/control_flow.rs
+++ src/bin/control_flow.rs
@@ -1,10 +1,13 @@
 //! Learning basic control flow: if else
+#![allow(clippy::comparison_chain)]
 
 use std::io;
 
 /// ### Control Flow if...else if...else...
+///
 /// Just like what you would write in C/C++:
-/// ```
+///
+/// ```c
 /// int x;
 /// scanf("%d", &x); // C
 /// std::cin >> x; // C++
@@ -19,14 +22,18 @@ use std::io;
 ///     std::cout << "The number is just 5!" << std::endl; // C++
 /// }
 /// ```
+///
 /// In Rust, just do it the same but remember to eliminate "()"!
+///
 /// ```
 /// let x: i32;
 /// if x < 5 {
 ///     println!("The number is smaller than 5");
 /// }
 /// ```
+///
 /// Implement function cf_if which:
+///
 /// 1. take a number x (i32) from stdin.
 /// 2. Compare it with 5, print "Less", "Equal", "Greater" (remember newline)
 /// according to the result of comparing x with 5.
--- src/bin/hello_world.rs
+++ src/bin/hello_world.rs
@@ -1,10 +1,11 @@
 //! Run a "Hello, world!" program.
+//!
 /// 1. Choose a directory on the computer to hold your project.
 /// 2. `cd` to that directory and run `cargo init`.
 /// 3. Use your favorite editor or IDE to open the source file located
 /// at `./src/main.rs`. (All editors and IDEs are the best editor/IDE
 /// on the world!)
-/// 4. Write a main function with only one line of code: 
+/// 4. Write a main function with only one line of code:
 /// ```
 /// println!("Hello, world!");
 /// ```
@@ -12,5 +13,5 @@
 /// 6. Run command `cargo run` to actually run your program.
 /// 7. Expect "Hello, world!" to be shown.
 fn main() {
-
+    println!("Hello, world!");
 }
