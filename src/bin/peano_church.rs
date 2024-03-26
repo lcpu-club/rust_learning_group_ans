41477de1641a2d237a829e9d020b33de9597e403
thezzisu <thezzisu@gmail.com>
2024-03-26 19:22:34 +0800

    chore(peano): updated statements

--- src/bin/peano_church.rs
+++ src/bin/peano_church.rs
@@ -4,39 +4,39 @@
 /// ### How should you complete this section?
 /// Just read the file, complete it and run. You could submit this file once
 /// you see `All tests passed` after running `cargo run`.
-/// 
+///
 /// ### Is Rust a functional programming language?
 /// 1. No, Rust is a instructional programming language.
-///    (1) Because we have all these loops, variables and endless side-
+///    - (1) Because we have all these loops, variables and endless side-
 ///        effects.
-///    (2) Function is not first-class member in Rust. If we want to
+///    - (2) Function is not first-class member in Rust. If we want to
 ///        pass a function as an argument, you'll need keywords `dyn`
 ///        and some wrapping helpers like `Rc` and `Box`, just to make
 ///        compiler happy. After all, rustc need to arrange memory
 ///        layout for you.
-///    (3) ...
+///    - (3) ...
 ///
 /// 2. Eh, Rust is like a functional programming language in some aspects.
-///    (1) Rust have powerful functional-like functionalities (Hahaha).
+///    - (1) Rust have powerful functional-like functionalities (Hahaha).
 ///        Like some common methods `for_each`, `map`, `reduce`, `filter`
 ///        on iterators, and `match` pattern of course, which is one of
 ///        the kernel concepts in common functional programming languages,
 ///        and Rust integrated them into itself very well.
-///    (2) When programming with Rust, you think differently from program-
+///    - (2) When programming with Rust, you think differently from program-
 ///        ming with C. When using C, you should always keep in your mind
 ///        the layout of memory, which pointer points to heap, which memory
 ///        should be freed to make your program behave well. In a word, you
 ///        are manipulating "memory" in C. But in Rust, you're always mani-
 ///        pulating `value` instead of "memory" itself directly. I mean,
 ///        Rust has provided you with a layer of abstraction.
-/// 
+///
 /// ### How to define a natural number?
 /// A Peano definition of natural number is: a natural number is either zero,
 /// or successor of another natural number.
-/// 
+///
 /// So it's clear how to define a Peano style natural number representation in Rust.
 /// We choose to use `enum` as it coult hold different `states` of a type.
-/// 
+///
 /// ```no_run
 /// #[derive(Debug, Clone, PartialEq)]
 /// enum Peano {
@@ -46,21 +46,21 @@
 /// ```
 /// We could implement `From<usize>` trait for our `enum Peano`.
 /// Yes, you could implement trait for enum as well, not only struct.
-/// 
+///
 /// There'are so many traits in Rust standard library for you to explore.
 /// For now let's learn `From` trait and `Into` trait first.
-/// 
+///
 /// As we have known, `trait` defines a set of shared behavior of different types.
 /// For example, we want to get `usize` value from different types. We may want to
 /// turn a numeral `42` into String "42". Or, we want to extract a numeral 42 directly
 /// from a JSON stream. Or, we could turn a Rust native `usize` value into Peano-style
 /// natural number. Then we could implement `From<usize>` trait for them.
-/// ```
+/// ```rust
 /// impl From<usize> for String;
 /// impl Into<usize> for JsonStream; // Pseudo type `JsonStream`.
 /// impl From<usize> for Peano;
 /// ```
-/// 
+///
 /// Complete these two implementations.
 /// ```no_run
 /// impl From<usize> for Peano {
@@ -74,16 +74,16 @@
 ///     }
 /// }
 /// ```
-/// 
+///
 /// ### Normal implementation.
-/// 
+///
 /// For a Peano number, we could find its predecessor and successor with method
 /// `pred` and `succ`.
 /// For example, predecessor of Peano 3 is Peano 2. And successor of Peano 3 is
 /// Peano 4.
-/// 
+///
 /// We define 0's predecessor as 0 arbitrarily.
-/// 
+///
 /// Complete the definition of `pred` and `succ` here. You may want to use `match`
 /// pattern for this exercise. (`match` pattern is powerful!)
 /// ```no_run
@@ -97,36 +97,36 @@
 /// }
 /// ```
 /// So what is trait `Add`? It describes the behavior when we apply `+` to
-/// two Peano numbers. More generally speaking, `Add<T>` describes the beha-
-/// vior when we apply `+` to two `T` typed values.
+/// two Peano numbers. More generally speaking, `Add<T>` describes the behavior
+/// when we apply `+` to two `T` typed values.
 ///
 /// It's definition is:
-/// ```
+/// ```rust
 /// trait Add {
 ///     type Output;
 ///     fn add(self, rhs: Self) -> Self::Output;
 /// }
 /// ```
-/// 
-/// 
+///
+///
 /// `type Output` is "associated type" of this trait. In our example, it
 /// describes "what type will we get when applying `+` to two Peano numbers?".
 ///
 /// So we could say, `type Output = Peano`, which means "we will get a Peano
 /// numeral number by adding two Peano numeral numbers"!
-/// 
+///
 /// Then `fn add` tell Rust how to add these two values.
 /// After implementing `Add` for Peano, this piece of code should pass test.
-/// 
-/// ```
+///
+/// ```rust
 /// let peano_2: Peano = 2.into();
 /// let peano_3: Peano = 3.into();
 /// let peano_5: Peano = 5.into();
 /// assert_eq!(peano_2 + peano_3, peano_5);
 /// ```
-/// 
+///
 /// It's like rewriting `operator+` in C++, isn't it?
-/// 
+///
 /// ```no_run
 /// impl Add for Peano {
 ///     type Output = Peano;
@@ -140,7 +140,7 @@
 ///     }
 /// }
 /// ```
-/// 
+///
 /// Similarly, trait `Sub` describes how to apply `-` to `enum Peano`.
 /// Complete the implementation. Don't forget `type Output`.
 /// ```no_run
@@ -152,13 +152,13 @@
 /// But implementing multiplication of Peano numbers might be tricky.
 /// Hint: it's ok to use recursion and previously implemented traits. And you
 /// may want to break multiplication into many additions.
-/// 
+///
 /// ```no_run
 /// impl Mul for Peano {
 ///     todo!()
 /// }
 /// ```
-/// 
+///
 /// You should pass following tests after successfully implemented contents above.
 /// ```no_run
 /// mod test_peano {
@@ -185,36 +185,36 @@
 ///     }
 /// }
 /// ```
-/// 
+///
 /// ### Church numeral
 /// Church numeral is yet another way to define natural number, but it originates
 /// from Lambda calculus and is based on pure function.
-/// 
+///
 /// **A Church number is a function that "takes a function and returns another function".**
 ///
 /// It would be super easy to write out its definition in a language where function
 /// is first-class member.
 ///
-/// ```
+/// ```rust
 /// type Church = function(function)->function; // Pseudo code, don't try to run it.
 /// ```
-/// 
+///
 /// But, how could we pass a function as an argument in Rust? Use trait `Fn` of
 /// course. You should be aware of the fact that we aren't going to use `FnOnce` and
 /// `FnMut` because we require that function passed as arguments could be called
 /// arbitrary times and we do not need mutability.
-/// 
+///
 /// However, `Fn` is just a trait, not a concrete type. So we should not pass "Fn",
 /// but "a value with a type that implements trait `Fn`".
 /// Here's two choices, using `impl` or using `dyn`. `impl` uses static dispatch,
 /// and `dyn` uses dynamic dispatch.
 /// We choose `dyn` here. So a type that implements trait `Fn` could be written
 /// as `dyn Fn` anonymously.
-/// 
-/// ```
+///
+/// ```rust
 /// type Church = dyn Fn(dyn Fn) -> dyn Fn; // Pseudo code, don't try to run it.
 /// ```
-/// 
+///
 /// But here's a problem: a dynamic dispatched value's size could not be known
 /// in compile time. That's to say, rustc do not know its memory layout and thus
 /// couldn't allocate memory for it statically.
@@ -222,8 +222,8 @@
 /// allocate memory on heap. We do not need that extra behavior. `Rc` is enough.
 /// (Remember that `Rc` is a smart pointer with fixed size regardless what type
 /// it wraps.) You should make compiler happy, always.
-/// 
-/// ```
+///
+/// ```rust
 /// type Church = Rc<dyn Fn(Rc<dyn Fn>) -> Rc<dyn Fn>>; // Pseudo code.
 /// ```
 ///
@@ -233,17 +233,17 @@
 /// definition of Church numerals, so we use a Generic Type `T` to indicate that
 /// we do not care much about what type does the function apply and return, we
 /// only need a `function`.
-/// 
-/// ```
+///
+/// ```rust
 /// // Turn `Fn` into `Fn(T)->T`.
 /// pub type Church = Rc<dyn Fn(Rc<dyn Fn(T) -> T>) -> Rc<dyn Fn(T) -> T>>;
 /// ```
-/// 
+///
 /// So generic type could be used when don't want/needto care a specific type.
 ///
 /// To illustrate this long type definition more clearly, we could expand this
 /// line into several lines.
-/// ```
+/// ```rust
 /// pub type Church<T> =
 ///     Rc<                             // Wrapper to make compiler happy.
 ///         dyn Fn                      // A function
@@ -251,26 +251,26 @@
 ///           -> Rc<dyn Fn(T) -> T>     // and returns another function.
 ///     >;
 /// ```
-/// 
+///
 /// ```no_run
 /// pub type Church<T> = Rc<dyn Fn(Rc<dyn Fn(T) -> T>) -> Rc<dyn Fn(T) -> T>>;
 /// ```
-/// 
+///
 /// Zero is a natural number. So in Church numeral, it is a function.
 /// We can call `zero` to get a Church number `zero`.
-/// 
+///
 /// ```no_run
 /// pub fn zero<T: 'static>() -> Church<T> {
 ///    // This is a function(closure) that takes a function `_f` and returns
 ///    // another function `move |x| x`. Obviously this closure's type satisfies
 ///    // `Fn` trait because it doesn't have any side effect. And the whole
 ///    // closure's type satisfies `Fn` trait as well.
-///    // 
+///    //
 ///    // `zero` is to apply a function `f` to `x` zero time.
 ///    Rc::new(move |_f| Rc::new(move |x| x))
 /// }
-/// 
-/// 
+///
+///
 /// // `one` is to apply a function `f` to `x` once.
 /// pub fn one<T: 'static>() -> Church<T> {
 ///     Rc::new(move |f| Rc::new(move |x| f(x)))
@@ -286,15 +286,15 @@
 ///     todo!()
 /// }
 /// ```
-/// 
+///
 /// Compute the successor of a Church number. Remember that the successor
 /// of a natural number is a natural number, so you should expect to get
 /// a Church number after calling `succ`.
-/// 
+///
 /// Remember that a Church number is a function that takes a function and
 /// returns a function. So you should give a closure that takes a function
 /// or closure and return a function or closure.
-/// 
+///
 /// Hint: successor of `n` is `n+1`. `n` means we have called function `f` on
 /// `x` for n times, and we should call `f` on `x` once again to get `n+1`.
 /// ```no_run
@@ -302,9 +302,9 @@
 ///     todo!()
 /// }
 /// ```
-/// 
+///
 /// Conversion between Rust `usize` and `Church`.
-/// 
+///
 /// Why couldn't we just write "impl<T> From<usize> for Church<T>"?
 /// Try it, look at what compiler of IDE says and get some knowledge of
 /// `encapsulation` in Rust!
@@ -316,7 +316,7 @@
 ///     }
 ///     result
 /// }
-/// 
+///
 /// // Same reason, we cannot write `impl<T> Into<usize> for Church<T>`.
 /// pub fn to_usize<T: 'static + Default>(n: Church<T>) -> usize {
 ///     let count = Rc::new(RefCell::new(0));
@@ -370,7 +370,7 @@
 ///     todo!()
 /// }
 /// ```
-/// 
+///
 /// You should pass following tests after implementing contents above.
 /// ```no_run
 /// mod test_church {
@@ -482,7 +482,7 @@
 ///     church_mult_2_3();
 ///     church_exp_2_3();
 ///     church_exp_3_5();
-/// 
+///
 ///     println!("All tests passed!");
 /// }
 /// ```
@@ -494,8 +494,8 @@ use std::{
 
 #[derive(Debug, Clone, PartialEq)]
 enum Peano {
-    O,              // Zero is natural number.
-    S(Rc<Peano>),   // Successor of a natural number is a natural number.
+    O,            // Zero is natural number.
+    S(Rc<Peano>), // Successor of a natural number is a natural number.
 }
 
 impl Peano {
@@ -593,27 +593,26 @@ mod test_peano {
 pub type Church<T> = Rc<dyn Fn(Rc<dyn Fn(T) -> T>) -> Rc<dyn Fn(T) -> T>>;
 
 pub fn zero<T: 'static>() -> Church<T> {
-
     // This is a function(closure) that takes a function `_f` and returns
     // another function `move |x| x`. Obviously this closure's type satisfies
     // `Fn` trait because it doesn't have any side effect. And the whole
     // closure's type satisfies `Fn` trait as well.
-    // 
+    //
     // `zero` is to apply a function `f` to `x` zero time.
     Rc::new(move |_f| Rc::new(move |x| x))
 }
 
-/// `one` is to apply a function `f` to `x` once.
+// `one` is to apply a function `f` to `x` once.
 pub fn one<T: 'static>() -> Church<T> {
     Rc::new(move |f| Rc::new(move |x| f(x)))
 }
 
-/// `two` is to apply a function `f` to `x` twice.
+// `two` is to apply a function `f` to `x` twice.
 pub fn two<T: 'static>() -> Church<T> {
     Rc::new(move |f| Rc::new(move |x| f(f(x))))
 }
 
-/// `three` is to apply a function `f` to `x` three times.
+// `three` is to apply a function `f` to `x` three times.
 pub fn three<T: 'static>() -> Church<T> {
     Rc::new(move |f| Rc::new(move |x| f(f(f(x)))))
 }
@@ -633,7 +632,7 @@ pub fn from_usize<T: 'static>(n: usize) -> Church<T> {
     result
 }
 
-/// Same reason, we cannot write `impl<T> Into<usize> for Church<T>`.
+// Same reason, we cannot write `impl<T> Into<usize> for Church<T>`.
 pub fn to_usize<T: 'static + Default>(n: Church<T>) -> usize {
     let count = Rc::new(RefCell::new(0));
     let c = Rc::clone(&count);
@@ -669,8 +668,8 @@ pub fn to_usize<T: 'static + Default>(n: Church<T>) -> usize {
     result
 }
 
-/// `add` is to add two Church numbers `n` and `m`.
-/// i.e. call `f` on `x` n times, and then another `m` times.
+// `add` is to add two Church numbers `n` and `m`.
+// i.e. call `f` on `x` n times, and then another `m` times.
 pub fn add<T: 'static>(n: Church<T>, m: Church<T>) -> Church<T> {
     Rc::new(move |f| {
         // Apply `f` n times first.
@@ -681,7 +680,7 @@ pub fn add<T: 'static>(n: Church<T>, m: Church<T>) -> Church<T> {
     })
 }
 
-/// `mult`. Applying "calling `f` on `x` n times" m times.
+// `mult`. Applying "calling `f` on `x` n times" m times.
 pub fn mult<T: 'static>(n: Church<T>, m: Church<T>) -> Church<T> {
     Rc::new(move |f| {
         // Apply `f` n times first.
@@ -692,8 +691,8 @@ pub fn mult<T: 'static>(n: Church<T>, m: Church<T>) -> Church<T> {
     })
 }
 
-/// `exp`. Most difficult one.
-/// Well, try to get some inspiration from the type annotaion of `m`.
+// `exp`. Most difficult one.
+// Well, try to get some inspiration from the type annotaion of `m`.
 pub fn exp<T: 'static>(n: Church<T>, m: Church<Rc<dyn Fn(T) -> T>>) -> Church<T> {
     Rc::new(move |f| {
         let n: Church<T> = n.clone();
@@ -783,12 +782,11 @@ mod test_church {
 
     pub fn church_exp_3_5() {
         let church_3: Church<T> = from_usize(3);
-        let church_5: Church<Rc<dyn Fn(T)->T>> = from_usize(5);
+        let church_5: Church<Rc<dyn Fn(T) -> T>> = from_usize(5);
         let church_243: Church<T> = from_usize(243);
         assert_eq!(to_usize(church_243), to_usize(exp(church_3, church_5)))
     }
 }
-
 
 fn main() {
     use test_peano::*;
